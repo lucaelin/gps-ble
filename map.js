d8307040bfd5d766ae4d86d80fa00ff579c1eeab
lucaelin <git@luca.lsys.ac>
2020-06-18 08:45:28 +0200

    fixed time of undefined bug

--- map.js
+++ map.js
@@ -89,7 +89,9 @@ export function plotPosition(gpsData, realtime=false) {
   }
 
   // TODO optimize to insert at correct position instead of sorting
-  if (path.data[path.data.length].time < gpsData.time) {
+  if (!path.data.length) {
+    path.data.push(gpsData);
+  } else if (path.data[path.data.length].time < gpsData.time) {
     path.data.push(gpsData);
   } else if (gpsData.time < path.data[0].time) {
     path.data.unshift(gpsData);
